74f5e95c2eb76c4f2ccbe9af3aef191cf1e6b737
licetus <licetus0316@gmail.com>
2018-12-30 13:32:39 +0800

    writing server file, create config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-import { PgManager, PgManagerConfig } from 'task-manager-model-ts'
+import express from 'express'
+import fileUpload from 'express-fileupload'
+import cors from 'cors'
+import swaggerTools from 'swagger-tools'
+import log4js from 'log4js'
+import { PgManager, Task } from 'task-manager-model-ts'
+
+log4js.configure({
+  appenders: {
+    out: { type: 'stdout', level: 'trace' },
+    logFile: {
+      type: 'dateFile', filename: 'logs/api', pattern: 'yyyyMMdd.log', alwaysIncludePattern: true, level: 'trace',
+    },
+  },
+  categories: {
+    default: { appenders: ['logFile', 'out'], level: 'trace' },
+  },
+})
+export const logger = log4js.getLogger()
+
+const server = async(isDev: boolean, isTest: boolean) => {
+  if (isDev) {
+    logger.level = 'INFO'
+  }
+
+  let { port } = config.server
+}
